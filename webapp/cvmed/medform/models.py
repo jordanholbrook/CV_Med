dc6ceb53e2851bacd92d30ed591d13bf77fee563
Johnny <johnvjlang@gmail.com>
2024-09-26 20:59:30 -0600

    serializers + class + model capture

--- webapp/cvmed/medform/models.py
+++ webapp/cvmed/medform/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class ImageClassification(models.Model):
+    image = models.ImageField(upload_to='images')
+    result = models.CharField(max_length=2, blank=True)
+    updated = models.DateField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+    # def save(self, *args, **kwargs):
+    #     return str(self.id)
--- webapp/cvmed/medform/views.py
+++ webapp/cvmed/medform/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .serializers import ImageSerializer
+from .models import ImageClassification
 
-def members(request):
+def medcv(request):
+  serializer_class = ImageSerializer
+  model_query = ImageClassification.objects.all()
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
